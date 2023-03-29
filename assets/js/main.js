02303665adf7ac4ed47468e1568995720d3c76a3
Jorge Freitas <jorgeslfreitas@gmail.com>
2023-03-28 22:58:11 -0300

    Update do JSON e arquivo JS para buscar dados do JSON
    Ajustes do JSON e arquivo JS para buscar dados do JSON

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,39 @@
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+
+    const phone = document.getElementById('profile.phone')
+    phone.innerText = profileData.phone
+
+    const whatsapp = document.getElementById('profile.whatsapp')
+    whatsapp.href = profileData.whatsapp
+
+    const email = document.getElementById('profile.email')
+    email.innerText = profileData.email
+
+    const linkedin = document.getElementById('profile.linkedin')
+    linkedin.href = profileData.linkedin
+
+    const github = document.getElementById('profile.github')
+    github.href = profileData.github
+
+    const curriculo = document.getElementById('profile.curriculo')
+    curriculo.href = profileData.curriculo
+
+}
+
 (async () => {
     const profileData = await fetchProfileData();
+    updateProfileInfo(profileData)
     console.log(profileData);
 })()
