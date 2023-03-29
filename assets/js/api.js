5e986107b61376bc51401433d60ef178c5d6ab53
Jorge Freitas <jorgeslfreitas@gmail.com>
2023-03-28 22:00:53 -0300

    Update api.json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/jorgeslfreitas/portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/jorgeslfreitas/portfolio-consumo-de-api/main/data/profile.json';
     const fetching = await fetch(url);
     return await fetching.json();
 }
